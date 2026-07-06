3e93cf7db47a5e0fa8fffdf3a9a68c93366073cb
erzan12 <do.earljan@gmail.com>
2026-07-06 16:49:23 +0800

    config: set up prisma, main.ts and migration file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', // your Next.js frontend
+  //   methods: 'GET,POST',
+  //   credentials: true,
+  // });
+
+  await app.listen(3002, () => {
+    console.log('Server is running at http://localhost:3002');
+  })
+
 }
 bootstrap();
